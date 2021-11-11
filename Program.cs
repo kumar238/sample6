9a75e7f9ff667e60a059f81a22dfebe31ac356e1
KUmAR DARivADA <kumardarivada@gmail.com>
2021-11-11 19:57:15 +0530

    apples

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, kumar");
+Console.WriteLine("Apples");
+Console.WriteLine("Banana");
